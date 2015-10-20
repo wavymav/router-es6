8cde420d8ba3a4c41cfb63fc1ce0c0abdf5dd190
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-19 23:25:27 -0400

    listing request methods & routes

--- router.js
+++ router.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const http = require('http');
+
+// Router
+function router(req, res) {
+    console.log('Requested route: ' + req.url);
+    console.log('Request method: ' + req.method);
+    res.writeHead(200, {'Content-type' : 'text/html'});
+    res.end('<h1>Hello Web</h1>')
+}
+
+// Creating the server
+http.createServer(router).listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
